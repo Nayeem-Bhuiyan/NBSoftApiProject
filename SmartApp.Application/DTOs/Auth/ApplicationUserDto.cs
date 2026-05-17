15451c7ad3a00cd3797d01e5cf002831a8a9730b
Nayeem Bhuiyan <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2026-05-17 18:36:09 +0600

    Change User Dto

--- SmartApp.Application/DTOs/Auth/ApplicationUserDto.cs
+++ SmartApp.Application/DTOs/Auth/ApplicationUserDto.cs
@@ -8,7 +8,6 @@ namespace SmartApp.Application.DTOs.Auth
 {
  public class ApplicationUserDto
     {
-        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
