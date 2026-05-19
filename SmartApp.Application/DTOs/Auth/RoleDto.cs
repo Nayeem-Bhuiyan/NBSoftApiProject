701976ede61ccbd32f69491ac4e7cad2d905b27b
Nayeem Bhuiyan <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2026-05-19 16:32:26 +0600

    RolePermission Feature generated Files

---
+++ SmartApp.Application/DTOs/Auth/RoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartApp.Application.DTOs.Auth;
+
+public sealed class RoleDto
+{
+    public string Id { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+}
